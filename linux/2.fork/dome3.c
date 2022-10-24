978e3defd1499f524499d1e93948613685c9b54e
yikelizi <927770271@qq.com>
2022-10-24 13:31:20 +0800

    验证问题

--- linux/2.fork/dome3.c
+++ linux/2.fork/dome3.c
@@ -5,23 +5,25 @@ int main()
 {
 	pid_t pida = 0;
 	pid_t pidb = 0;
+ 	int data = 10;
+
 
 	pida = getpid();
 	printf("before fork pid = %d\n",pida);
 
 	pidb = fork();
 
-	printf("after fork pid = %d\n",getpid());
-
-	if(getpid() > 0)
+	if(pidb > 0)
 	{
 		printf("father fork,%d\n",getpid());
 	}
 	else{
 
 		printf("child fork,%d\n",getpid());
+		data += 10;
         }
-
+	
+	printf("data=%d,地址是%p这个\n",data,&data);
 	return 0;
 }
 
--- linux/2.fork/dome4.c
+++ linux/2.fork/dome4.c
@@ -5,23 +5,26 @@ int main()
 {
 	pid_t pida = 0;
 	pid_t pidb = 0;
+ 	int data;
+
 
 	pida = getpid();
 	printf("before fork pid = %d\n",pida);
 
 	pidb = fork();
 
-	printf("after fork pid = %d\n",getpid());
-
-	if(getpid() > 0)
+	if(pidb > 0)
 	{
 		printf("father fork,%d\n",getpid());
+		data = 10;
 	}
 	else{
 
 		printf("child fork,%d\n",getpid());
+		data = 20;
         }
-
+	
+	printf("data=%d,地址是%p这个\n",data,&data);
 	return 0;
 }
 
